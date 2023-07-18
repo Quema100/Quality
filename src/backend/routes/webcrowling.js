717a110d6b352703e34c9c6968d62e8ed16d689e
Quema <myoon0929@gmail.com>
2023-07-19 00:10:13 +0900

    add

--- src/backend/routes/webcrowling.js
+++ src/backend/routes/webcrowling.js
@@ -14,13 +14,14 @@ function webcrowling(app,fs,path,os,axios,cheerio){
           users = JSON.parse(fileContent);
         }
       }
+      const filepath = req.query.filepath || null;
       const userID = req.query.userID;      
       const errorMessage = 'You have accessed an invalid path.'
 
       if(!userID || userID !== users.userid){
-          return res.redirect('/signin?errorMessage=' + encodeURIComponent(errorMessage))
+        return res.redirect('/signin?errorMessage=' + encodeURIComponent(errorMessage))
       }else{
-        res.render(path.join(__dirname, '../../view', 'webcrowling.ejs'),{userID:userID});
+        return res.render(path.join(__dirname, '../../view', 'webcrowling.ejs'),{userID:userID,filepath:filepath});
       }
     })
     .post((req, res) => {
@@ -28,6 +29,12 @@ function webcrowling(app,fs,path,os,axios,cheerio){
       const first = req.body.mainhtmlCode
       const second = req.body.secondhtmlCode
       const third = req.body.thirdhtmlCode
+      const qualityDir = `/Users/${username}/Documents/Qualitycrowling`; // Quality 폴더 경로
+      const filePath = path.join(qualityDir, 'crowlinginfo.json'); // users.json 파일 경로
+
+      if (!fs.existsSync(qualityDir)) {
+        fs.mkdirSync(qualityDir);
+      }
 
       const gethtml= async(first,second,third) => {
         try{
@@ -47,6 +54,18 @@ function webcrowling(app,fs,path,os,axios,cheerio){
             };
           });
             console.log("bodyList : ", ulList);
+            fs.writeFile(filePath, JSON.stringify(ulList), 'utf-8', (err) => {
+              if (err) {
+                console.log(err);
+                return res.status(500).send('Error occurred while saving user data.');
+              }
+              console.log('New info added to the file.');
+
+              const userID = req.body.userID;
+              const filepath = `${filePath}`
+
+              res.redirect('/signin/contents/crowling?userID=' + encodeURIComponent(userID) + '&filepath=' + encodeURIComponent(filepath))            
+            });
 
           })
         }catch(err){
@@ -55,10 +74,6 @@ function webcrowling(app,fs,path,os,axios,cheerio){
       }
 
       gethtml(first,second,third)
-
-      const userID = req.body.userID;
-
-      res.redirect('/signin/contents/crowling?userID=' + encodeURIComponent(userID))
     })
 }
 
