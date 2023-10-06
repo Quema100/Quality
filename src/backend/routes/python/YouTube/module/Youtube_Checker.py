9efdca069231560b670bffbe7e4fe9dca94f56bb
Quema <myoon0929@gmail.com>
2023-10-06 13:37:42 +0900

    add vido checker.bat and add clean terminal code

--- src/backend/routes/python/YouTube/module/Youtube_Checker.py
+++ src/backend/routes/python/YouTube/module/Youtube_Checker.py
@@ -1,6 +1,9 @@
+import os
 import yt_dlp
 
 def youtube_info(url):
+    os.system('cls' if os.name == 'nt' else 'clear')
+
     ydl_opts = {
         'quiet': True,
         'listformats': True,    
